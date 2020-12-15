ccfd375889fb415220d97056be8729682fb4ad1c
ns3838 <72807027+ns3838@users.noreply.github.com>
2020-12-15 11:03:43 -0500

    Update pxt.json, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 while (true) {
     console.log("Light Level:" + input.lightLevel())
+    if (input.lightLevel() < 20) {
+        light.setAll(light.rgb(0, 0, 255))
+    } else {
+        light.setAll(light.rgb(0, 0, 0))
+    }
+    
 }
